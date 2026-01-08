341ffe161b396d00368ce72a2a339eeee3da4acc
Leonardo Perassoli <leonardoperass@gmail.com>
2026-01-08 15:42:59 -0300

    feat(photos): add PhotoWidget component with loading state and skeleton UI

--- src/pages/page-componets.tsx
+++ src/pages/page-componets.tsx
@@ -10,7 +10,7 @@ import SearchIcon from "../assets/icons/search.svg?react"
 import InputCheckbox from "../components/input-checkbox";
 import InputSingleFile from "../components/input-single-file";
 import {useForm} from "react-hook-form";
-import ImageFilePreview from "../components/image-file-preview";
+import ImagePreview from "../components/image-preview";
 import { Dialog, DialogClose, DialogTrigger } from "@radix-ui/react-dialog";
 import { DialogBody, DialogContent, DialogFooter, DialogHeader } from "../components/dialog";
 import Text from "../components/text";
@@ -75,7 +75,7 @@ export default function PageComponents() {
 					form={form}
 					allowedExtensions={['png', 'jpg', 'jpeg', 'webp']}
 					maxFileSizeInMB={50}
-					replaceBy={<ImageFilePreview src={fileSource} alt="Imagem" />}
+					replaceBy={<ImagePreview src={fileSource} alt="Imagem" />}
 					{...form.register('file')} 
 				/>
 			</div>
@@ -95,7 +95,7 @@ export default function PageComponents() {
 								form={form}
 								allowedExtensions={['png', 'jpg', 'jpeg', 'webp']}
 								maxFileSizeInMB={50}
-								replaceBy={<ImageFilePreview src={fileSource} alt="Imagem" />}
+								replaceBy={<ImagePreview src={fileSource} alt="Imagem" />}
 								{...form.register('file')} 
 							/>
 						</DialogBody>
--- src/pages/page-home.tsx
+++ src/pages/page-home.tsx
@@ -1,9 +1,53 @@
 import Text from "../components/text";
+import PhotoWidget from "../contexts/photos/models/components/photo-widget";
+import Container from "../components/container";
+import type { Photo } from "../contexts/photos/models/photo";
 
 export default function PageHome() {
     return (
-        <>
-            <Text variant="heading-medium">Página Inicial!</Text>
-        </>
+        <Container>
+            <div className="grid grid-cols-4 gap-9">
+                <PhotoWidget
+                    photo={{
+                        id: '123',
+                        title: 'Olá mundo',
+                        imageId: "portrait-tower.png",
+                        albums: [
+                            {id:'321', title: 'Album 1'},
+                            {id:'123', title: 'Album 2'},
+                            {id:'456', title: 'Album 3'},
+                        ]
+                    }}
+                />
+                <PhotoWidget
+                    photo={{
+                        id: 'abc',
+                        title: 'Olá mundo',
+                        imageId: "square-cat.png",
+                        albums: [
+                            {id:'321', title: 'Album 1'},
+                            {id:'123', title: 'Album 2'},
+                            {id:'456', title: 'Album 3'},
+                        ]
+                    }}
+                />
+                <PhotoWidget
+                    photo={{
+                        id: '456',
+                        title: 'Olá mundo',
+                        imageId: "wide-cafeteria.png",
+                        albums: [
+                            {id:'321', title: 'Album 1'},
+                            {id:'123', title: 'Album 2'},
+                            {id:'456', title: 'Album 3'},
+                        ]
+                    }}
+                />
+                <PhotoWidget 
+                    photo={{} as Photo}
+                    loading
+                />
+            </div>
+        </Container>
     )
 }
