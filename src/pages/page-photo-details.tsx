03dd9bac281154915bdd24546d952278f5c0f530
Leonardo Perassoli <leonardoperass@gmail.com>
2026-01-17 18:55:47 -0300

    feat(photo-details): add photo details page layout and navigation component

--- src/pages/page-photo-details.tsx
+++ src/pages/page-photo-details.tsx
@@ -1,15 +1,38 @@
 import { useParams } from "react-router";
 import Text from "../components/text";
+import Container from "../components/container";
+import type { Photo } from "../contexts/photos/models/photo";
+import Skeleton from "../components/skeleton";
+import PhotosNavigator from "../contexts/photos/models/components/photos-navigator";
 
 
 export default function PagePhotoDetails() {
     const {id} = useParams();
 
+    // just for mock test
+    const isLoadingPhoto = false;
+    const photo = {
+        id: "123",
+        title: "Olá mundo!",
+        imageId: "portrait-tower.png",
+        albums: [
+            { id: "3421", title: "Album 1" },
+            { id: "3421", title: "Album 2" },
+            { id: "3421", title: "Album 3" },
+        ]
+    } as Photo;
+
     return (
-        <>
-            <Text variant="heading-medium">Página detalhe da foto</Text>
-            <hr />
-            <Text variant="heading-medium">ID da Foto: {id}</Text>
-        </>
+        <Container>
+            <header className="flex items-center justify-between gap-8 mb-8">
+                {!isLoadingPhoto ? (
+                    <Text variant="heading-large">{photo?.title}</Text>
+                ) : (
+                    <Skeleton className="w-48 h-8"/>
+                )}
+
+                <PhotosNavigator />
+            </header>
+        </Container>
     );
 }
