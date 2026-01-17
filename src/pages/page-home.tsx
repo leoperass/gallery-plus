6629870e0213d7b08b24501bd01f4ab15127f187
Leonardo Perassoli <leonardoperass@gmail.com>
2026-01-17 10:28:28 -0300

    feat(albums): add AlbumsFilter component with loading state

--- src/pages/page-home.tsx
+++ src/pages/page-home.tsx
@@ -1,19 +1,29 @@
 import Container from "../components/container";
+import AlbumsFilter from "../contexts/photos/models/albums/components/albums-filter";
 import PhotosList from "../contexts/photos/models/components/photos.list";
 
 export default function PageHome() {
     return (
         <Container>
-            <PhotosList 
+            <AlbumsFilter 
+                albums={[
+                    { id: "3421", title: "Album 1" },
+                    { id: "3421", title: "Album 2" },
+                    { id: "3421", title: "Album 3" },
+                ]}
+                className="mb-9" 
+            />
+
+            <PhotosList
                 photos={[
                     {
                         id: "123",
                         title: "Olá mundo!",
                         imageId: "portrait-tower.png",
                         albums: [
-                            {id: "3421", title: "Album 1"},
-                            {id: "3421", title: "Album 2"},
-                            {id: "3421", title: "Album 3"},
+                            { id: "3421", title: "Album 1" },
+                            { id: "3421", title: "Album 2" },
+                            { id: "3421", title: "Album 3" },
                         ],
                     },
                     {
@@ -21,9 +31,9 @@ export default function PageHome() {
                         title: "Olá mundo!",
                         imageId: "portrait-tree.png",
                         albums: [
-                            {id: "3421", title: "Album 1"},
-                            {id: "3421", title: "Album 2"},
-                            {id: "3421", title: "Album 3"},
+                            { id: "3421", title: "Album 1" },
+                            { id: "3421", title: "Album 2" },
+                            { id: "3421", title: "Album 3" },
                         ],
                     },
                 ]}
