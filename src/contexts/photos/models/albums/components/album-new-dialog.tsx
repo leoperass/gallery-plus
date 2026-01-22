1c835751f5e4788044fc4698d79ce05787b369fe
Leonardo Perassoli <leonardoperass@gmail.com>
2026-01-22 14:49:19 -0300

    feat(album): add new album creation dialog with photo selection preview

---
+++ src/contexts/photos/models/albums/components/album-new-dialog.tsx
@@ -0,0 +1,97 @@
+import Button from "../../../../../components/button";
+import { Dialog, DialogBody, DialogClose, DialogContent, DialogFooter, DialogHeader, DialogTrigger } from "../../../../../components/dialog";
+import InputText from "../../../../../components/input-text";
+import Text from "../../../../../components/text";
+import type { Photo } from "../../photo";
+import SelectCheckBoxIllustration from "../../../../../assets/images/select-checkbox.svg?react";
+import Skeleton from "../../../../../components/skeleton";
+import ImagePreview from "../../../../../components/image-preview";
+
+interface AlbumNewDialogProps {
+    trigger: React.ReactNode;
+}
+
+export default function AlbumNewDialog({ trigger }: AlbumNewDialogProps) {
+    //TODO: Use API when it`s ready 
+    const photos: Photo[] = [
+        {
+            id: "123",
+            title: "Olá mundo!",
+            imageId: "portrait-tower.png",
+            albums: [
+                { id: "3421", title: "Album 1" },
+                { id: "3421", title: "Album 2" },
+                { id: "3421", title: "Album 3" },
+            ],
+        },
+        {
+            id: "123",
+            title: "Olá mundo!",
+            imageId: "portrait-tree.png",
+            albums: [
+                { id: "3421", title: "Album 1" },
+                { id: "3421", title: "Album 2" },
+                { id: "3421", title: "Album 3" },
+            ],
+        },
+    ]
+    const isLoadingPhotos = false;
+
+    return (
+        <Dialog>
+            <DialogTrigger asChild>{trigger}</DialogTrigger>
+            <DialogContent>
+                <DialogHeader>Criar álbum</DialogHeader>
+
+                <DialogBody className="flex flex-col gap-6">
+                    <InputText placeholder="Adicione um título" />
+
+                    <div className="space-y-6">
+
+                        <Text as="div" variant="label-small">Fotos cadastradas</Text>
+
+                        {!isLoadingPhotos && photos.length > 0 && (
+                            <div className="flex flex-wrap gap-3">
+                                {photos.map((photo) => (
+                                    <ImagePreview
+                                        key={photo.id}
+                                        src={`/images/${photo.imageId}`}
+                                        title={photo.title}
+                                        className="w-20 h-20"
+                                    />
+                                ))}
+                            </div>
+                        )}
+
+                        {isLoadingPhotos &&
+                            <div className="flex flex-wrap gap-3">
+                                {Array.from({ length: 4 }).map((_, index) => (
+                                    <Skeleton
+                                        key={`photo-loading-${index}`}
+                                        className="w-20 h-20 rounded-lg"
+                                    />
+                                ))}
+                            </div>
+                        }
+
+
+                        {!isLoadingPhotos && photos.length === 0 && (
+                            <div className="flex flex-col justify-center items-center gap-6">
+                                <SelectCheckBoxIllustration />
+                                <Text variant="paragraph-medium" className="text-center">Nenhuma foto disponível para seleção</Text>
+                            </div>
+                        )}
+                    </div>
+                </DialogBody>
+
+                <DialogFooter>
+                    <DialogClose asChild>
+                        <Button variant="secondary">Cancelar</Button>
+                    </DialogClose>
+
+                    <Button>Criar</Button>
+                </DialogFooter>
+            </DialogContent>
+        </Dialog>
+    )
+}
