3b0849e57b492fefc8653483109b69e46ed2d93e
Leonardo Perassoli <leonardoperass@gmail.com>
2026-02-26 14:31:43 -0300

    feat(album): implement Zod schema for album form validation

--- src/contexts/photos/models/albums/components/album-new-dialog.tsx
+++ src/contexts/photos/models/albums/components/album-new-dialog.tsx
@@ -1,3 +1,4 @@
+import React from "react";
 import Button from "../../../../../components/button";
 import { Dialog, DialogBody, DialogClose, DialogContent, DialogFooter, DialogHeader, DialogTrigger } from "../../../../../components/dialog";
 import InputText from "../../../../../components/input-text";
@@ -6,75 +7,97 @@ import SelectCheckBoxIllustration from "../../../../../assets/images/select-chec
 import Skeleton from "../../../../../components/skeleton";
 import PhotoImageSelectable from "../../components/photo-image-selectable";
 import usePhotos from "../../hooks/use-photos";
+import { useForm } from "react-hook-form";
+import { albumNewFormSchema, type AlbumNewFormSchema } from "../schemas";
+import { zodResolver } from "@hookform/resolvers/zod";
 
 interface AlbumNewDialogProps {
     trigger: React.ReactNode;
 }
 
 export default function AlbumNewDialog({ trigger }: AlbumNewDialogProps) {
-    
+    const [modalOpen, setModalOpen] = React.useState(false);
+    const form = useForm<AlbumNewFormSchema>({
+        resolver: zodResolver(albumNewFormSchema)
+    });
     const {photos, isLoadingPhotos} = usePhotos();
+
+    React.useEffect(() => {
+        if (!modalOpen) {
+            form.reset();
+        }
+    }, [modalOpen, form])
 
     function handleTogglePhoto(selected: boolean, photoId: string) {
         console.log(selected, photoId)
     }
+
+    function handleSubmit(payload: AlbumNewFormSchema) {
+        console.log(payload);
+    }
     
 
     return (
-        <Dialog>
+        <Dialog open={modalOpen} onOpenChange={setModalOpen}>
             <DialogTrigger asChild>{trigger}</DialogTrigger>
             <DialogContent>
-                <DialogHeader>Criar álbum</DialogHeader>
+                <form onSubmit={form.handleSubmit(handleSubmit)}>
+                    <DialogHeader>Criar álbum</DialogHeader>
 
-                <DialogBody className="flex flex-col gap-6">
-                    <InputText placeholder="Adicione um título" />
+                    <DialogBody className="flex flex-col gap-6">
+                        <InputText
+                            placeholder="Adicione um título" 
+                            error={form.formState.errors.title?.message}
+                            {...form.register('title')}
+                        />
 
-                    <div className="space-y-6">
+                        <div className="space-y-6">
 
-                        <Text as="div" variant="label-small">Fotos cadastradas</Text>
+                            <Text as="div" variant="label-small">Fotos cadastradas</Text>
 
-                        {!isLoadingPhotos && photos.length > 0 && (
-                            <div className="flex flex-wrap gap-3">
-                                {photos.map((photo) => (
-                                    <PhotoImageSelectable 
-                                        key={photo.id}
-                                        src={`${import.meta.env.VITE_IMAGES_URL}/${photo.imageId}`}
-                                        title={photo.title}
-                                        imageClassName="w-20 h-20"
-                                        onSelectImage={(selected) => handleTogglePhoto(selected,photo.id)}
-                                    />
-                                ))}
-                            </div>
-                        )}
+                            {!isLoadingPhotos && photos.length > 0 && (
+                                <div className="flex flex-wrap gap-3">
+                                    {photos.map((photo) => (
+                                        <PhotoImageSelectable 
+                                            key={photo.id}
+                                            src={`${import.meta.env.VITE_IMAGES_URL}/${photo.imageId}`}
+                                            title={photo.title}
+                                            imageClassName="w-20 h-20"
+                                            onSelectImage={(selected) => handleTogglePhoto(selected,photo.id)}
+                                        />
+                                    ))}
+                                </div>
+                            )}
 
-                        {isLoadingPhotos &&
-                            <div className="flex flex-wrap gap-3">
-                                {Array.from({ length: 4 }).map((_, index) => (
-                                    <Skeleton
-                                        key={`photo-loading-${index}`}
-                                        className="w-20 h-20 rounded-lg"
-                                    />
-                                ))}
-                            </div>
-                        }
+                            {isLoadingPhotos &&
+                                <div className="flex flex-wrap gap-3">
+                                    {Array.from({ length: 4 }).map((_, index) => (
+                                        <Skeleton
+                                            key={`photo-loading-${index}`}
+                                            className="w-20 h-20 rounded-lg"
+                                        />
+                                    ))}
+                                </div>
+                            }
 
 
-                        {!isLoadingPhotos && photos.length === 0 && (
-                            <div className="flex flex-col justify-center items-center gap-6">
-                                <SelectCheckBoxIllustration />
-                                <Text variant="paragraph-medium" className="text-center">Nenhuma foto disponível para seleção</Text>
-                            </div>
-                        )}
-                    </div>
-                </DialogBody>
+                            {!isLoadingPhotos && photos.length === 0 && (
+                                <div className="flex flex-col justify-center items-center gap-6">
+                                    <SelectCheckBoxIllustration />
+                                    <Text variant="paragraph-medium" className="text-center">Nenhuma foto disponível para seleção</Text>
+                                </div>
+                            )}
+                        </div>
+                    </DialogBody>
 
-                <DialogFooter>
-                    <DialogClose asChild>
-                        <Button variant="secondary">Cancelar</Button>
-                    </DialogClose>
+                    <DialogFooter>
+                        <DialogClose asChild>
+                            <Button variant="secondary">Cancelar</Button>
+                        </DialogClose>
 
-                    <Button>Criar</Button>
-                </DialogFooter>
+                        <Button type="submit">Criar</Button>
+                    </DialogFooter>
+                </form>
             </DialogContent>
         </Dialog>
     )
